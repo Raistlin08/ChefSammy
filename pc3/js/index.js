077f0018232152d3bcbef44ebe32719eaacb7b5f
Tommaso Vicario <tommaso.vicario@gmail.com>
2026-03-29 00:07:18 +0100

    Added fetch to server with user message

--- pc3/js/index.js
+++ pc3/js/index.js
@@ -1,4 +1,6 @@
-function send(){
+const llm = "http://localhost:8080/chat"
+
+async function send(){
     let chatdiv = document.getElementById("chatdiv");
     let textfield = document.getElementById("textfield");
     let message = textfield.value.trim();
@@ -7,4 +9,25 @@ function send(){
     textfield.value = "";
     chatdiv.innerHTML += '<p class="user-message">' + message + '</p>'
     chatdiv.scrollTop = chatdiv.scrollHeight;
+
+    try {
+        let answer = await fetch(llm, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({"user": message})
+        });
+
+        if (!answer.ok) {
+            throw new Error(`server error: ${answer.status}`);
+        }
+        let data = await answer.json()
+        let llm_message = data["ai"]
+        chatdiv.innerHTML += '<p class="ai-message">' + llm_message + '</p>'
+        chatdiv.scrollTop = chatdiv.scrollHeight;
+
+    } catch (error) {
+        console.error("Problem with fetch:", error);
+        chatdiv.innerHTML += `<p class="ai-message" style="color: red;">Errore di connessione.</p>`;
+    }
+
 }
