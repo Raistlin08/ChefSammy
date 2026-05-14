60e979ebf0626e5ab0eb0d8fc4bb99566dbebec4
MorlundicusXVII <mouhamadoumandiang27@gmail.com>
2026-05-14 10:03:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const llm = "/chat"
+
+async function send(){
+    let chatdiv = document.getElementById("chatdiv");
+    let textfield = document.getElementById("textfield");
+    let message = textfield.value.trim();
+    if (message === "") return;
+
+    textfield.value = "";
+    chatdiv.innerHTML += '<p class="user-message">' + message + '</p>'
+    chatdiv.scrollTop = chatdiv.scrollHeight;
+
+    try {
+        let answer = await fetch(llm, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({"user": message})
+        });
+        let data = await answer.json()
+        if (!answer.ok) {
+            throw new Error(`server error: ${data.error}`);
+        }
+        
+        let llm_message = data["ai"]
+        chatdiv.innerHTML += '<p class="ai-message">' + llm_message + '</p>'
+        chatdiv.scrollTop = chatdiv.scrollHeight;
+
+    } catch (error) {
+        console.error("Problem with fetch:", error);
+        chatdiv.innerHTML += `<p class="ai-message" style="color: red;">${error}</p>`;
+    }
+
+}
